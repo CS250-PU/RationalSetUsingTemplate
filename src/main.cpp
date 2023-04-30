75f856a776a3e733615226e555d88ae7b24db2ef
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2023-04-30 14:48:49 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//******************************************************************************
+// File name:   main.cpp
+// Author:      
+// Date:        
+// Class:       
+// Assignment:  
+// Purpose:    
+// Hours: 
+//******************************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+  return EXIT_SUCCESS;
+}
